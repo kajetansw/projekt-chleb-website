322e1583e6490bf51fa45c8b2a45f933fed71d33
kajetan.swiatek <kajetan.swiatek@aptitudesoftware.com>
2021-01-05 20:12:41 +0100

    Initial commit from Create Next App

---
+++ test/pages/index.test.tsx
@@ -0,0 +1,12 @@
+import { fireEvent, render } from '@testing-library/react';
+import React from 'react';
+import { Home } from '../../pages/index';
+
+describe('Home page', () => {
+  it('clicking button triggers alert', () => {
+    const { getByText } = render(<Home />, {});
+    window.alert = jest.fn();
+    fireEvent.click(getByText('Test Button'));
+    expect(window.alert).toHaveBeenCalledWith('With typescript and Jest');
+  });
+});
