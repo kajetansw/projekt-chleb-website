0df164d7d4b372aa2951a081e55aba807dbf2875
kajetan.swiatek <kajetan.swiatek@aptitudesoftware.com>
2021-01-13 20:07:05 +0100

    set up react-hook-form for AddRecipeModal's form

--- components/AddRecipeModal/index.tsx
+++ components/AddRecipeModal/index.tsx
@@ -22,12 +22,23 @@ import {
   useDisclosure,
 } from '@chakra-ui/react';
 import { AddIcon } from '@chakra-ui/icons';
-import { useRef } from 'react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+import { Recipe } from '@/models';
+
+type FormFieldValues = Omit<Recipe, 'uid' | 'likes'>;
 
 const AddRecipeModal = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { register, handleSubmit, getValues } = useForm<FormFieldValues>();
+  const [checkboxGroupValue, setCheckboxGroupValue] = useState<React.ReactText[]>([]);
 
-  const initialRef = useRef(null!);
+  const createRecipe = () => {
+    const recipeToSave = { ...getValues(), tags: checkboxGroupValue };
+    // @TODO make ingredients an array instead string
+    // @TODO save it to Firestore
+  };
 
   return (
     <>
@@ -35,61 +46,72 @@ const AddRecipeModal = () => {
         <AddIcon mr={3} /> Dodaj przepis
       </Button>
 
-      <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Dodaj przepis</ModalHeader>
           <ModalCloseButton />
-          <ModalBody pb={6}>
-            <FormControl>
-              <FormLabel>Tytuł</FormLabel>
-              <Input ref={initialRef} placeholder="Tytuł" />
-            </FormControl>
 
-            <FormControl mt={4}>
-              <FormLabel>Składniki</FormLabel>
-              <Input placeholder="Składniki" />
-            </FormControl>
+          <form id="addRecipeForm" onSubmit={handleSubmit(createRecipe)}>
+            <ModalBody pb={6}>
+              <FormControl>
+                <FormLabel>Tytuł</FormLabel>
+                <Input ref={register} name="title" placeholder="Tytuł" />
+              </FormControl>
 
-            <FormControl mt={4}>
-              <FormLabel>Instrukcja</FormLabel>
-              <Textarea rows={5} placeholder="Instrukcja" />
-            </FormControl>
+              <FormControl mt={4}>
+                <FormLabel>Składniki</FormLabel>
+                <Input ref={register} name="ingredients" placeholder="Składniki" />
+              </FormControl>
 
-            <FormControl mt={4}>
-              <FormLabel>Czas przygotowania (w min.)</FormLabel>
-              <NumberInput step={5} defaultValue={15} min={0} max={1000}>
-                <NumberInputField />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-            </FormControl>
+              <FormControl mt={4}>
+                <FormLabel>Instrukcja</FormLabel>
+                <Textarea ref={register} name="instruction" rows={5} placeholder="Instrukcja" />
+              </FormControl>
 
-            <FormControl mt={4}>
-              <FormLabel>URL zdjęcia</FormLabel>
-              <Input placeholder="URL zdjęcia" />
-            </FormControl>
+              <FormControl mt={4}>
+                <FormLabel>Czas przygotowania (w min.)</FormLabel>
+                <NumberInput
+                  name="timeOfPreparationInMins"
+                  step={5}
+                  defaultValue={15}
+                  min={0}
+                  max={1000}
+                >
+                  <NumberInputField ref={register} />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
+              </FormControl>
 
-            <FormControl mt={4}>
-              <FormLabel>Tagi</FormLabel>
-              <CheckboxGroup>
-                <SimpleGrid columns={3} spacingY={2}>
-                  <Checkbox value="Chleb jasny">Chleb jasny</Checkbox>
-                  <Checkbox value="Chleb ciemny">Chleb ciemny</Checkbox>
-                  <Checkbox value="Deser">Deser</Checkbox>
-                  <Checkbox value="Przekąska">Przekąska</Checkbox>
-                </SimpleGrid>
-              </CheckboxGroup>
-            </FormControl>
-          </ModalBody>
+              <FormControl mt={4}>
+                <FormLabel>URL zdjęcia</FormLabel>
+                <Input ref={register} name="imageSrc" placeholder="URL zdjęcia" />
+              </FormControl>
+
+              <FormControl mt={4}>
+                <FormLabel>Tagi</FormLabel>
+                <CheckboxGroup onChange={setCheckboxGroupValue}>
+                  <SimpleGrid columns={3} spacingY={2}>
+                    <Checkbox value="Chleb jasny">Chleb jasny</Checkbox>
+                    <Checkbox value="Chleb ciemny">Chleb ciemny</Checkbox>
+                    <Checkbox value="Deser">Deser</Checkbox>
+                    <Checkbox value="Przekąska">Przekąska</Checkbox>
+                  </SimpleGrid>
+                </CheckboxGroup>
+              </FormControl>
+            </ModalBody>
+          </form>
 
           <ModalFooter>
             <Button onClick={onClose} mr={3}>
               Cancel
             </Button>
-            <Button colorScheme="blue">Save</Button>
+            <Button colorScheme="blue" type="submit" form="addRecipeForm">
+              Save
+            </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
