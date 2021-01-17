89116ed41d92f89ef16cbabb490d1f76533601df
kajetan.swiatek <kajetan.swiatek@aptitudesoftware.com>
2021-01-17 18:22:47 +0100

    sanitized escaped characters for Firebase private key (CICD problem)

--- scripts/addClaims.ts
+++ scripts/addClaims.ts
@@ -7,7 +7,7 @@ if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     }),
     databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
