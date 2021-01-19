0a6b8c9f12c7ccf83f2ec34b3fade0724f69e075
kajetan.swiatek <kajetan.swiatek@aptitudesoftware.com>
2021-01-19 20:15:16 +0100

    added onerror handling for ResponsiveImage component

--- components/ResponsiveImage/index.tsx
+++ components/ResponsiveImage/index.tsx
@@ -1,5 +1,6 @@
 import { useBreakpointValue } from '@chakra-ui/react';
 import NextImage, { ImageProps as NextImageProps } from 'next/image';
+import { useState } from 'react';
 
 type ResponsiveImageProps = Omit<NextImageProps, 'width' | 'height'> & {
   width: Record<string, number | string> | number[];
@@ -9,8 +10,19 @@ type ResponsiveImageProps = Omit<NextImageProps, 'width' | 'height'> & {
 const ResponsiveImage = ({ width, height, ...nextImageProps }: ResponsiveImageProps) => {
   const breakpointWidth = useBreakpointValue(width) || 0;
   const breakpointHeight = useBreakpointValue(height) || 0;
+  const [imageSrc, setImageSrc] = useState(nextImageProps.src);
+  const { src, ...imagePropsWithoutSrc } = nextImageProps;
+  const onImageError = () => setImageSrc('/no-image.png');
 
-  return <NextImage {...nextImageProps} width={breakpointWidth} height={breakpointHeight} />;
+  return (
+    <NextImage
+      {...imagePropsWithoutSrc}
+      onError={onImageError}
+      src={imageSrc}
+      width={breakpointWidth}
+      height={breakpointHeight}
+    />
+  );
 };
 
 export default ResponsiveImage;
